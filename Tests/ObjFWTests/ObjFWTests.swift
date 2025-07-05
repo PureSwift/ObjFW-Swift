8d2e0e2770eb09555ebe6d89e7637f0f2d1952db
Alsey Coleman Miller <alseycmiller@gmail.com>
2025-07-05 06:35:43 +0000

    Updated unit tests

--- Tests/ObjFWTests/ObjFWTests.swift
+++ Tests/ObjFWTests/ObjFWTests.swift
@@ -12,26 +12,38 @@ import CObjFW
     #endif
     
     // Get OFString class
-    let ofStringClass: objfw_class = try #require(objfw_get_class(className))
+    let stringClass: objfw_class = try #require(objfw_get_class(className))
+    let stringMetaClass: objfw_class = try #require(objfw_get_meta_class(className))
     
     // Get selectors
-    let selStringWithUTF8 = try #require(objfw_register_selector("stringWithUTF8String:"))
-    let selUTF8String = try #require(objfw_register_selector("UTF8String"))
+    let selCreate = try #require(objfw_register_selector("stringWithUTF8String:"))
+    let selString = try #require(objfw_register_selector("UTF8String"))
     
     // Create the OFString instance
     let swiftStr = "Hello from Swift calling ObjFW C API!"
     let obj = swiftStr.withCString { cString in
-        objfw_msgSend_class_id_cstr(ofStringClass, selStringWithUTF8, cString) as objfw_id
+        let imp = class_getMethodImplementation(stringMetaClass, selCreate)
+        typealias MsgSendCreate = @convention(c) (objfw_class, objfw_sel, UnsafePointer<CChar>) -> objfw_id
+        let fn = unsafeBitCast(imp, to: MsgSendCreate.self)
+        return fn(stringClass, selCreate, cString)
     }
     
-    let cStr = objfw_msgSend_cstr(obj, selUTF8String) as UnsafePointer<Int8>
-    let message = String(cString: cStr)
+    let message: String
+    do {
+        let impUTF8 = class_getMethodImplementation(stringClass, selString)
+        typealias FnUTF8 = @convention(c) (objfw_id, objfw_sel) -> UnsafePointer<CChar>
+        let fnUTF8 = unsafeBitCast(impUTF8, to: FnUTF8.self)
+        let cStr = fnUTF8(obj, selString)
+        message = String(cString: cStr)
+    }
+    
     print("ObjFW says: \(message)")
 }
-/*
-@_silgen_name("objc_msgSend")
-func objfw_msgSend<T>(_ arguments: Any...) -> T
 
+//@_silgen_name("objc_msgSend")
+//func objfw_msgSend()
+
+/*
 // objfw_id objfw_msgSend_class_id_cstr(objfw_class cls, objfw_sel sel, const char *arg)
 @_silgen_name("objc_msgSend")
 func objfw_msgSend_class_id_cstr(_ class: objfw_class, _ sel: objfw_sel, _ cString: UnsafePointer<Int8>) -> objfw_id
