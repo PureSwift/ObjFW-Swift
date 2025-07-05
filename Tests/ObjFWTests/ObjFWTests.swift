a1fe001adae4e3bf32cbd51140b7ee60f63bbef1
Alsey Coleman Miller <alseycmiller@gmail.com>
2025-07-05 07:30:00 +0000

    Update unit tests

--- Tests/ObjFWTests/ObjFWTests.swift
+++ Tests/ObjFWTests/ObjFWTests.swift
@@ -6,49 +6,52 @@ import CObjFW
 @Test func string() async throws {
     
     #if canImport(Darwin)
-    let className = "NSString"
+    let className = "NSObject"
     #else
-    let className = "OFString"
+    let className = "OFObject"  // ObjFW NSString equivalent
     #endif
     
-    // Get OFString class
-    let stringClass: objfw_class = try #require(objfw_get_class(className))
-    let stringMetaClass: objfw_class = try #require(objfw_get_meta_class(className))
-    
-    // Get selectors
-    let selCreate = try #require(objfw_register_selector("stringWithUTF8String:"))
-    let selString = try #require(objfw_register_selector("UTF8String"))
-    
-    // Create the OFString instance
-    let swiftStr = "Hello from Swift calling ObjFW C API!"
-    let obj = swiftStr.withCString { cString in
-        let imp = class_getMethodImplementation(stringMetaClass, selCreate)
-        typealias MsgSendCreate = @convention(c) (objfw_class, objfw_sel, UnsafePointer<CChar>) -> objfw_id
-        let fn = unsafeBitCast(imp, to: MsgSendCreate.self)
-        return fn(stringClass, selCreate, cString)
+    // 1. Get the NSObject class
+    guard let objectClass = objfw_get_class(className) else {
+        print("Failed to get NSObject class")
+        return
     }
     
-    let message: String
-    do {
-        let impUTF8 = class_getMethodImplementation(stringClass, selString)
-        typealias FnUTF8 = @convention(c) (objfw_id, objfw_sel) -> UnsafePointer<CChar>
-        let fnUTF8 = unsafeBitCast(impUTF8, to: FnUTF8.self)
-        let cStr = fnUTF8(obj, selString)
-        message = String(cString: cStr)
+    // 2. Manually create instance (equivalent to alloc, but lower-level)
+    guard let obj = objfw_create_instance(objectClass, 0) else {
+        print("Failed to create instance")
+        return
     }
     
-    print("ObjFW says: \(message)")
+    // 3. Prepare selectors
+    let initSel = sel_registerName("init")
+    let descriptionSel = sel_registerName("description")
+    
+    // 4. Get IMP for -init
+    guard let initIMP = class_getMethodImplementation(objectClass, initSel) else {
+        print("Failed to get IMP for init")
+        return
+    }
+    
+    // 5. Get IMP for -description
+    guard let descIMP = class_getMethodImplementation(objectClass, descriptionSel) else {
+        print("Failed to get IMP for description")
+        return
+    }
+    
+    // 6. Define the correct function pointer type:
+    typealias MethodCall = @convention(c) (objfw_id, objfw_sel) -> objfw_id
+    
+    let initFunction = unsafeBitCast(initIMP, to: MethodCall.self)
+    let descFunction = unsafeBitCast(descIMP, to: MethodCall.self)
+    
+    // 7. Call [obj init]
+    let initializedObj = initFunction(obj as! objfw_id, initSel)
+    
+    // 8. Call [initializedObj description]
+    let descriptionObj = descFunction(initializedObj, descriptionSel)
+    
+    // 9. Print result
+    print(descriptionObj)
+
 }
-
-//@_silgen_name("objc_msgSend")
-//func objfw_msgSend()
-
-/*
-// objfw_id objfw_msgSend_class_id_cstr(objfw_class cls, objfw_sel sel, const char *arg)
-@_silgen_name("objc_msgSend")
-func objfw_msgSend_class_id_cstr(_ class: objfw_class, _ sel: objfw_sel, _ cString: UnsafePointer<Int8>) -> objfw_id
-
-// const char *objfw_msgSend_cstr(objfw_id obj, objfw_sel sel)
-@_silgen_name("objc_msgSend")
-func objfw_msgSend_cstr(_ obj: objfw_id, _ sel: objfw_sel) -> UnsafePointer<Int8>
-*/
