892ca3c82e8e647038c90c638e9da14b6338b66d
Alsey Coleman Miller <alseycmiller@gmail.com>
2025-07-05 07:30:14 +0000

    Update `objfw_create_instance`

--- Sources/CObjFW/shim.h
+++ Sources/CObjFW/shim.h
@@ -32,9 +32,18 @@ static inline objfw_sel objfw_register_selector(const char *name) {
     return sel_registerName(name);
 }
 
+#if __APPLE__
+#else
+extern id OFAllocObject(objfw_class class_, size_t extraSize, size_t extraAlignment, void *_Nullable *_Nullable extra);
+#endif
+
 // Shims for object instantiation
-static inline objfw_id objfw_create_instance(objfw_class cls, size_t extraBytes) {
-    return class_createInstance(cls, extraBytes);
+static inline objfw_id _Nullable objfw_create_instance(objfw_class class_, size_t extraSize) {
+#if __APPLE__
+    return class_createInstance(class_, extraSize);
+#else
+    return OFAllocObject(class_, extraSize, 0, nil);
+#endif
 }
 
 // Shims for adding methods
@@ -45,4 +54,23 @@ static inline BOOL objfw_class_add_method(
     const char *types
 ) {
     return class_addMethod(cls, name, imp, types);
+}
+
+id ofobject_create_and_init() {
+    Class cls = objc_getClass("OFObject");
+    if (!cls) {
+        [OFStdErr writeLine: @"OFObject class not found"];
+        return nil;
+    }
+
+    id obj = class_createInstance(cls, 0);
+    SEL initSel = sel_registerName("init");
+    obj = objc_msg_send(obj, initSel);
+
+    return obj;
+}
+
+id ofobject_description(id obj) {
+    SEL descSel = sel_registerName("description");
+    return objc_msg_send(obj, descSel);
 }
